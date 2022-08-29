a289f81d7be33b2e8fac93a29064fe7b32e4351d
ahmeddahees <ahmeddahees7@gmail.com>
2022-08-29 22:14:00 +0200

    first commit

---
+++ js.js
@@ -0,0 +1,130 @@
+const show = document.createElement("h3")
+const foterdiv = document.querySelector(".foterdiv")
+const bt = document.querySelectorAll(".bt")
+const bigdiv = document.querySelector(".bigdiv")
+const sectinoblack = document.querySelector(".sectinoblack")
+
+//////////////////////////////
+const loop = () => {
+
+    let smalldiv = document.querySelectorAll(".smalldiv")
+    let total = 0
+    smalldiv.forEach(item => {
+
+        let dolar = Number(item.getElementsByClassName("dolar")[0].innerText)
+        let value = Number(item.getElementsByClassName("input")[0].value)
+        total = total + (dolar * value)
+
+        let totall = document.querySelector(".totall")
+        totall.innerText = total
+
+    });
+}
+
+
+bt.forEach((item) => {
+    item.addEventListener("click", () => {
+        item.classList.add("google")
+        item.innerHTML = "&#x2714; Done"
+            // item.append.innerText = "ss"
+            // item.style.backgroundColor = "rgb(0, 188, 44)"
+            // item.style.color = "white"
+        const body = document.getElementById("bodyy")
+        const creat = document.createElement("h1")
+        creat.innerText = "مبرووووووووووك "
+        body.prepend(creat)
+        creat.classList.add("ccreat")
+        setTimeout(() => {
+            creat.style.transform = "translateX(-200vw)"
+        }, 1000);
+        setTimeout(() => {
+            creat.remove()
+        }, 4000);
+        show.innerText = "عرض المشنريات "
+        body.prepend(show)
+        show.classList.add("sshow")
+
+        const p = item.parentElement.parentElement.getElementsByClassName("p")[0].innerText
+        const shirt = item.parentElement.parentElement.getElementsByClassName("shirt")[0].innerText
+        const bigimg = item.parentElement.parentElement.getElementsByClassName("bigimg")[0].src
+
+
+        const cc = document.querySelector(".cc")
+        cc.addEventListener("click", () => {
+
+            loop()
+
+        })
+
+
+
+
+        cc.innerHTML +=
+            `   <div  class="smalldiv"> 
+<button class="btnsmall">حذف</button> 
+<p class=   "dolar"   > ${p} </p> 
+<div class="divquantaty"> 
+<p>الكميه</p>
+<input class="input"  type="number"  min="1"  value="2">  
+</div>
+<div class=" Shirt"> 
+<p  class="pshirt">${shirt}</p>
+<img class="imgsmall"  src=${bigimg}>
+</div>
+`
+
+
+        cc.addEventListener("click", (eo) => {
+            if (eo.target.classList.contains("btnsmall")) {
+                eo.target.parentElement.remove()
+                total = 0
+
+                const pshirt = eo.target.parentElement.querySelector(".pshirt").innerText
+                const all = document.querySelectorAll(".divconten")
+                all.forEach(item => {
+                    const ite = item.getElementsByClassName("shirt")[0].innerText
+                    console.log(ite)
+                    if (pshirt == ite) {
+                        const bttt = item.getElementsByClassName("bt")[0];
+                        bttt.classList.remove("google");
+                        bttt.innerHTML = ("click")
+                    }
+
+                });
+            }
+        })
+
+
+    })
+
+
+
+
+
+    ////////////////
+    const x = document.querySelector(".x")
+    x.addEventListener("click", () => {
+        bigdiv.style.transform = "  translateX(100vw) "
+    })
+
+    show.addEventListener("click", () => {
+            bigdiv.style.transform = "  translateX(0vw) "
+
+        })
+        //////////////
+        ///////////////
+        /////////////////
+
+
+
+
+
+
+    // 
+
+    // console.log(pshirt);
+
+
+
+
+})
